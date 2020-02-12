ee5d32996254a61037b271c81840a0a73edca2d1
jekjekdagreyt <60175144+jekjekdagreyt@users.noreply.github.com>
2020-02-12 15:10:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-<?php
-			header("Content-type: text/xml");
-		 echo "<?xml version = '1.0' encoding = 'UTF-8'?>
-				<cars>";
+<html>
+	<title>Cars</title>
+	<body>
+			<?php
+				$object = new DOMDocument();
+		$object->load("https://examxxx.herokuapp.com/");
+	
+	
+		$content = $object->getElementsByTagName("item");
+	
+		foreach($content as $data){
+				$name = $data->getElementsByTagName("name")->item(0)->nodeValue;
+				$information = $data->getElementsByTagName("information")->item(0)->nodeValue;
+				$news = $data->getElementsByTagName("news")->item(0)->nodeValue;
+				$link = $data->getElementsByTagName("url")->item(0)->nodeValue;
+		
+			echo "$name - $information - $news - $link <br>";
+	}
+
 			
+			?>
 			
-			echo "<item>
-				<name>Ford Motors Company</name><br/>
-				<information>Ford Motor Company is a global company based in Dearborn, Michigan. The company designs, manufactures, markets and services a full line of Ford cars, trucks, SUVs, electrified vehicles and Lincoln luxury vehicles, provides financial services through Ford Motor Credit Company and is pursuing leadership positions in electrification; mobility solutions, including self-driving services; and connected services.  Ford employs approximately 190,000 people worldwide. </information><br/>
-				<news>FORD ANNOUNCES SENIOR LEADERSHIP CHANGES AS COMPANY CONTINUES TRANSFORMATION
-				DEARBORN, Feb. 7, 2020 – Ford Motor Company today announced that Jim Farley has been named chief operating officer of Ford, reporting to Jim Hackett, the company’s president and CEO, as Ford continues to carry out its Creating Tomorrow Together strategic plan.
-
-Ford also announced that Joe Hinrichs is retiring as Ford’s president, Automotive, after a highly accomplished 19-year career with the company. 
-
-The changes come as Ford is moving with urgency to fully integrate and accelerate its transformation into a higher-growth, higher-margin business by leveraging smart, connected vehicles and services. Since 2017, Ford has made important progress in transforming the company in the face of sweeping technological change and disruption in the auto industry while working to improve the fitness of its base business – restructuring operations, invigorating the product portfolio and reducing bureaucracy.
-
-“Jim Farley is the right person to take on this important new role,” Hackett said. “Jim’s passion for great vehicles and his intense drive for results are well known. He also has developed into a transformational leader with the imagination and foresight to help lead Ford into the future.” </news><br/>
-				<url><a href='https://media.ford.com/content/fordmedia/fna/us/en/news.html/' target = '_blank'>Ford Motors Company</a></url>
-			</item>";
-			echo "</cars>";
-	?>
+	</body>
+</html>
